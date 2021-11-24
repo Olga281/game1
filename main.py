63a01442bf6028c0d20a349f3529f8d3b574457e
Olga <egorova@yandexlyceum.ru>
2021-11-24 23:24:27 +0300

    клетки

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
 
-x, y = input().split()
-size = width, height = (int(x), int(y))
+a, b = (int(i) for i in input().split())
+size = width, height = (a, a)
 screen = pygame.display.set_mode(size)
 pygame.init()
-pygame.display.set_caption('Крест')
+pygame.display.set_caption('Шахматная клетка')
+screen.fill((0, 0, 0))
+n = a // b
+y = a
+num = 1
+while y > 0:
+    if num % 2 == 0:
+        x = 0
+    else:
+        x = n
+    while x <= a:
+        screen.fill(pygame.Color('white'), (x, y - n, n, n))
+        x = x + 2 * n
+    y = y - n
+    num = num + 1
 
-def draw():
-    screen.fill((0, 0, 0))
-    pygame.draw.line(screen, (255, 255, 255), [0, 0], [int(x), int(y)], 5)
-    pygame.draw.line(screen, (255, 255, 255), [0, int(y)], [int(x), 0], 5)
-
-draw()
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
